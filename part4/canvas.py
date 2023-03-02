2dbaa391b5e0c195f2e5bdbaa844f88598b8ff8e
VeeraNitish7 <nitish111cse@gmail.com>
2023-03-02 06:20:33 -0500

    Done part 4

--- part4/canvas.py
+++ part4/canvas.py
@@ -9,15 +9,19 @@ from tkinter import *
 from recognition import Recognizer
 from templates import *
 import time
+from PIL import ImageTk,Image
+from tkinter import ttk
 
 recognizer = Recognizer()
 for template in templates:
     recognizer.addTemplate(template)
-# templatenames = ['triangle', 'x', 'rectangle', 'circle', 'check', 'caret', 'zig_zag', 'arrow',
-#              'left_square_bracket', 'right_square_bracket', 'v', 'delete',
-#              'left_curly_brace', 'right_curly_brace', 'star', 'pigtail']
+templatenames = ['triangle', 'x', 'rectangle', 'circle', 'check', 'caret', 'zig_zag', 'arrow',
+             'left_square_bracket', 'right_square_bracket', 'v', 'delete',
+             'left_curly_brace', 'right_curly_brace', 'star', 'pigtail']
 
-templatenames = ['triangle', 'x', 'rectangle']
+
+
+
 
 
 class Screen():
@@ -32,6 +36,21 @@ class Screen():
 
         self.top = Tk()
 
+        # canvasImage = Canvas(self.top, width=100, height=100)
+        # canvasImage.grid()
+        # img = ImageTk.PhotoImage(Image.open("gestures.png"))
+        # canvasImage.create_image(1, 1, anchor=N, image=img)
+
+        def open_new_win():
+            top = Toplevel(self.top)
+            canvasImage = Canvas(self.canvas, height=180, width=100, bg="#aaaffe")
+            canvasImage.grid()
+            img = ImageTk.PhotoImage(Image.open("gestures.png"))
+            canvasImage.create_image(1, 1, anchor=N, image=img)
+
+            # Label(canvas1, text="You can modify this text", font='Helvetica 18 bold').grid()
+        # img = ImageTk.PhotoImage(Image.open("gestures.png"))
+        # self.top.create_image(2, 2, anchor=NW, image=img)
         """
             Creating variables to store the shape and score.
         """
@@ -50,6 +69,12 @@ class Screen():
 
 
         self.canvas = Canvas(self.top, width=500, height=500)
+
+        # button = ttk.Button(self.canvas, text="Open Window", command=open_new_win)
+        # button.grid()
+
+
+
         self.canvas.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
         self.button = Button(self.top, text='Clear', width=25, command=self.clear_btn_click)
         self.submit_button = Button(self.top, text='Submit', width=25, command=self.submit_btn_click)
@@ -63,10 +88,11 @@ class Screen():
         self.username_button.grid()
 
 
+
         self.button.grid()
         self.submit_button.grid()
         self.draw_button.grid()
-        self.inputtxt.grid()
+        # self.inputtxt.grid()
         """
             Binding various actions to the canvas.
         """
@@ -120,12 +146,12 @@ class Screen():
 
     def on_mouse_up(self, event):
         pass
-        # matched_template, score = recognizer.recognize(self.line_points)
-        #
-        # self.shape.set(matched_template.name)
-        # self.score.set("{0:.2f}".format(score * 100))
-        #
-        # self.line_points = []
+        matched_template, score = recognizer.recognize(self.line_points)
+
+        self.shape.set(matched_template.name)
+        self.score.set("{0:.2f}".format(score * 100))
+
+        self.line_points = []
 
     """
     'clear_btn_click' method is triggered when the user clicks the 'clear' button on the canvas. This allows the user to 
@@ -138,14 +164,17 @@ class Screen():
     def submit_btn_click(self):
         # print(self.gesturepoints)
 
+
+
         self.user = self.inputtxt.get(1.0, "end-1c")
         # print("The username is as follows : {}".format(self.username))
         self.allgestures[templatenames[self.templatecount] + str(self.counter).zfill(2)] = self.gesturepoints
         self.gesturepoints = []
-        if self.counter == 3:
+        if self.counter == 10:
             self.templatecount += 1
             self.counter = 0
         self.counter += 1
+        self.canvas.delete('all')
         try:
             self.shape.set(templatenames[self.templatecount])
         except IndexError:
@@ -169,13 +198,15 @@ class Screen():
         self.canvas.bind("<ButtonRelease-1>", self.dummy)
         self.on_right_click('')
         self.shape.set(templatenames[self.templatecount])
-        self.score.set(str((self.counter % 10) + 1))
+        self.score.set(self.counter)
 
     def get_username(self):
+        self.canvas.delete("userdisplay_label")
         username = self.username_entry.get()
         self.username = username
         display_message = 'The username is : {}'.format(username)
-        self.canvas.create_text(100, 100, text=display_message)
+        self.canvas.create_text(100, 100, text=display_message, tags=('userdisplay_label',))
+
 
     def dummy(self, event):
         pass
